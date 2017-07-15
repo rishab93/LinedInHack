147844d684c3cff27bedd13d0f97803d7760c038
Ziting Shen <zishen@zishen-mn1.linkedin.biz>
2017-07-15 12:51:53 -0700

    add recommendations

--- app/src/main/java/com/example/zishen/linkedinhack/RecommendClothingActivity.java
+++ app/src/main/java/com/example/zishen/linkedinhack/RecommendClothingActivity.java
@@ -2,9 +2,12 @@ package com.example.zishen.linkedinhack;
 
 import android.app.DatePickerDialog;
 import android.app.Dialog;
+import android.content.Intent;
 import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
 import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.TextView;
@@ -25,7 +28,13 @@ public class RecommendClothingActivity extends AppCompatActivity {
 
         eText.setText("Trip end date: " + prefs.getString("endDate", null));
 
-
+        Button myWishlist = (Button) findViewById(R.id.myWishlist);
+        myWishlist.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                startActivity(new Intent(RecommendClothingActivity.this, MyOutfit.class));
+            }
+        });
     }
 
 
