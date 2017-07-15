65bd1765fc6107dbf1d57f5a2a72fb909246f271
Ziting Shen <zishen@zishen-mn1.linkedin.biz>
2017-07-15 10:40:21 -0700

    change recommendation activity

--- app/src/main/java/com/example/zishen/linkedinhack/RecommendClothingActivity.java
+++ app/src/main/java/com/example/zishen/linkedinhack/RecommendClothingActivity.java
@@ -2,6 +2,7 @@ package com.example.zishen.linkedinhack;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.EditText;
 
 public class RecommendClothingActivity extends AppCompatActivity {
 
