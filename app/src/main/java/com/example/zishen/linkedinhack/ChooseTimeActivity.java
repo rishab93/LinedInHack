8465aee5b65bef10cdd891b2d24791cf049780c9
Ziting Shen <zishen@zishen-mn1.linkedin.biz>
2017-07-14 23:50:44 -0700

    change the name of MainActivity

--- app/src/main/java/com/example/zishen/linkedinhack/ChooseTimeActivity.java
+++ app/src/main/java/com/example/zishen/linkedinhack/ChooseTimeActivity.java
@@ -2,10 +2,7 @@ package com.example.zishen.linkedinhack;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.view.View;
 import android.widget.Button;
 
@@ -20,7 +17,7 @@ public class ChooseTimeActivity extends AppCompatActivity {
         finishEnterTime.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                startActivity(new Intent(ChooseTimeActivity.this, MainActivity.class));
+                startActivity(new Intent(ChooseTimeActivity.this, RecommendClothingActivity.class));
             }
         });
     }
